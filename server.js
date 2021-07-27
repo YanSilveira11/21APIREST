081564e29aac25b11c2a205c5cc539f10448c9f6
YanSilveira11 <silveirayan89@gmail.com>
2021-07-27 14:07:53 -0300

    atualizando package

---
+++ server.js
@@ -0,0 +1,67 @@
+/* 
+Descricao: Arquivo de configuração dos verbos HTTP
+Aluno: Yan Silveira de Souza
+Data: 27 / 07 / 2021
+*/
+
+const express = require('express');
+const app = express()
+const data = require("./data.json");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+// VERBOS HTTP
+// GET: Receber
+// POST: Enviar
+// PUT: Atualizar
+// DELETE: Deletar
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+app.get("/clientes", function (req, res) {
+  res.json(data);
+});
+
+app.get("/clientes/:id", function (req, res) {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  res.json(client);
+});
+
+app.post("/clientes", function (req, res) {
+  const { name, email } = req.body;
+
+  res.json({ name, email });
+});
+
+app.put("/clientes/:id", function (req, res) {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  const { name } = req.body;
+
+  client.name = name;
+
+  res.json(client);
+});
+
+app.delete("/clientes/:id", function (req, res) {
+  const { id } = req.params;
+  const clientsFiltered = data.filter(client => client.id != id);
+
+  res.json(clientsFiltered);
+});
+
+app.list(3000, function () {
+  console.log("Server is running");
+});
